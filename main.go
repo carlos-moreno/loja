88be04b2525e57c21e85e242f53445b44e179392
Carlos Moreno <omorenodovale@gmail.com>
2024-01-03 16:50:18 -0300

    feat: Adding struct Produto

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+type Produto struct {
+	Nome, Descricao string
+	Preco           float64
+	Quantidade      int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,5 +19,11 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 10},
+		{"Tênis", "Confortável", 99, 10},
+		{"Fone", "Muito bom", 149.90, 5},
+		{"Sapato", "Muito confortável", 189.90, 8},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
 }
